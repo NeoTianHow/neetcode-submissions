655ecd25686e26353e2a0ada66b8bc3ebcd1ebf0
Neo Tian How <43518077+NeoTianHow@users.noreply.github.com>
2026-07-18 15:35:17 +0800

    Update submission-18.py

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-18.py
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-18.py
@@ -3,17 +3,21 @@ class Solution:
         # Two passes: build prefix products, then multiply by postfix products.
         prefix = 1
         res = [1] * len(nums)
-
         for i in range(len(nums)):
+            # The first prefix will have a default value of 1 
             res[i] = prefix
+            # Prepare for the next number
             prefix *= nums[i]
+        
 
         postfix = 1
-
-        for i in range(len(nums) - 1, -1, -1):
+        for i in range(len(nums)-1,-1,-1):
+            # The first postfix will have a default value of 1.
+            # But, we also need to multiply it with the prefix to get
+            # the final result for each position
             res[i] *= postfix
+            # Prepare for the next number
             postfix *= nums[i]
-
         return res
     
     # Pattern: Prefix & Suffix Products
